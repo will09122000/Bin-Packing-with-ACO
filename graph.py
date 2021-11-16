455a4f67c2bff3e669f9b3fc62695dcec477d943
Will <will0912@icloud.com>
2021-11-16 18:40:40 +0000

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ class Graph:
 
     def __init__(self, bins, items):
         self.nodes = self.create_nodes(bins, items)
-        self.create_edges(items)
+        self.create_edges(bins, items)
 
     def create_nodes(self, bins, items):
         nodes = []
@@ -12,30 +12,31 @@ class Graph:
 
         for item in range(0, items):
             for bin in range(0, bins):
-                nodes.append(Node(item, bin))
+                nodes.append(Node(bin, item))
 
         # End node.
         nodes.append(Node(None, None))
 
         return nodes
 
-    def create_edges(self, items):
+    def create_edges(self, bins, items):
 
         # Start node.
-        next_bin = 0
-        for destination_node in self.nodes:
-            if destination_node.bin == next_bin:
-                edge = Edge(destination_node)
-                self.nodes[0].edges.append(edge)
+        for destination_node in self.nodes[1:bins+1]:
+            edge = Edge(destination_node)
+            self.nodes[0].edges.append(edge)
 
+        # Iterate through all nodes that are not the start or end nodes.
+        next_item = 0
         for i, node in enumerate(self.nodes[1:-1]):
-            #print(node.bin, node.item)
-            if node.bin != items-1:
-                next_bin = node.bin + 1
+            if node.item != items-1:
+                next_item = node.item + 1
+                #for destination_node in self.nodes[i+1:i+bins+1]:
                 for destination_node in self.nodes:
-                    if destination_node.bin == next_bin:
+                    if destination_node.item == next_item:
                         edge = Edge(destination_node)
                         node.edges.append(edge)
+            # Bins for the last item all point to the end node.
             else:
                 edge = Edge(self.nodes[-1])
                 node.edges.append(edge)
@@ -52,6 +53,7 @@ class Edge:
         self.destination = destination
         self.pheromone = 0.1 if pheromone is None else pheromone
 
+
 graph = Graph(3, 5)
 
 for i, node in enumerate(graph.nodes):
@@ -60,3 +62,4 @@ for i, node in enumerate(graph.nodes):
         print('Edges:')
         for edge in node.edges:
             print(edge.destination.bin, edge.destination.item)
+    print(" ")
